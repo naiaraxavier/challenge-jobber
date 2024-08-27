3949b373c9bdd3de40047f15c2dd2333008b429e
Naiara Martins <naiaraxf@gmail.com>
2024-08-27 19:00:38 -0300

    feat: add main content header with styles

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,28 @@
 import { Plus } from "lucide-react";
 import { Button } from "../components/button";
 import { Header } from "../components/header";
+import "../css/home-page.css";
 
 export function HomePage() {
   return (
     <>
       <Header />
 
-      <main>
-        <div>
-          <div>
-            <div>
-              <h2>Lista de trabalhos</h2>
-            </div>
+      <main className="main-content">
+        <div className="main-content__container">
+          <div className="main-content__header">
+            <h2>Lista de trabalhos</h2>
             <Button size="1">
               <Plus size={20} strokeWidth={3} />
-              Cadastrar trabalho
+              Cadastrar
             </Button>
           </div>
+
+          <ul className="main-content__title">
+            <li>Título</li>
+            <li>Descrição</li>
+            <li>Criado em</li>
+          </ul>
         </div>
       </main>
 
