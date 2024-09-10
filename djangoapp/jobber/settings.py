3c88656fdcc74f322e9751f84ab6a76df6d5cc0b
Naiara Martins <naiaraxf@gmail.com>
2024-09-10 18:13:39 -0300

    fix: config db in settings

--- djangoapp/jobber/settings.py
+++ djangoapp/jobber/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
