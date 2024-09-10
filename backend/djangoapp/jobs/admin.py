e5600cbc5353225f89ad426df762595c16bb2f9e
Naiara Martins <naiaraxf@gmail.com>
2024-09-10 15:55:28 -0300

    feat: preparing environment for deployment

--- backend/djangoapp/jobs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Jobs
-
-admin.site.site_header = "Avanti Challenge Jobber"
-admin.site.register(Jobs)
--- frontend/src/utils/format-date.ts
+++
@@ -1,5 +0,0 @@
-// Formata a data que vem da API
-export const formatDate = (dateString: string): string => {
-  const [year, month, day] = dateString.split("-");
-  return `${day}/${month}/${year}`;
-};
