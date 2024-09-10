0539be6a3516faf644c4628596a89e77c182a649
Naiara Martins <naiaraxf@gmail.com>
2024-09-10 18:56:51 -0300

    Fix PORT configuration in sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -13,4 +13,4 @@ echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:$PORT
+python manage.py runserver
