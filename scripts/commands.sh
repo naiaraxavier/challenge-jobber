e01ec00089622dd37a6eca1c84f96ca593a3fe6f
Naiara Martins <naiaraxf@gmail.com>
2024-09-10 19:17:29 -0300

    Update port

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,6 +3,8 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
 
+echo "PORT is set to: $PORT"
+
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
   sleep 2
@@ -10,7 +12,6 @@ done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
-echo "PORT is set to: $PORT"
 
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
