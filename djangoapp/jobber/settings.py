18ca6d6f1d9087416b51ddf13b7bcd83de353226
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 17:09:42 -0300

    update settings

--- djangoapp/jobber/settings.py
+++ djangoapp/jobber/settings.py
@@ -114,6 +114,7 @@ if ON_HEROKU:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 else:
     STATIC_URL = "/static/"
     MEDIA_URL = "/media/"
