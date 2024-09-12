39a2e60e2852c4fa5e7df2ebfec8ee8a9d39b66a
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 20:15:14 -0300

    Update settings

--- djangoapp/jobber/settings.py
+++ djangoapp/jobber/settings.py
@@ -123,22 +123,20 @@ AWS_S3_FILE_OVERWRITE = False
 
 
 #STATIC FILES
-if ON_HEROKU:
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-    STATIC_ROOT = '/tmp/static'
-    MEDIA_ROOT = '/tmp/media'
+# if ON_HEROKU:
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+STATIC_ROOT = '/tmp/static'
+MEDIA_ROOT = '/tmp/media'
 
-else:
-    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-    STATIC_URL = "/static/"
-    MEDIA_URL = "/media/"
-    STATIC_ROOT = DATA_DIR / "static"
-    MEDIA_ROOT = DATA_DIR / "media"
+# else:
+#     # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+#     STATIC_URL = "/static/"
+#     MEDIA_URL = "/media/"
+#     STATIC_ROOT = DATA_DIR / "static"
+#     MEDIA_ROOT = DATA_DIR / "media"
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
