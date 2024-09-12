71f4e17d72d818742b1ff724cf06c500e5935688
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 16:46:11 -0300

    update settings

--- djangoapp/jobber/settings.py
+++ djangoapp/jobber/settings.py
@@ -80,22 +80,22 @@ logger.info(f"ON_HEROKU: {ON_HEROKU}")
 
 if ON_HEROKU:
     # Configurações específicas para o Heroku
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
+    # AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    # AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    # AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    # AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
 
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
-    AWS_DEFAULT_ACL = None
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_FILE_OVERWRITE = False
+    # AWS_DEFAULT_ACL = None
+    # AWS_QUERYSTRING_AUTH = False
+    # AWS_S3_FILE_OVERWRITE = False
 
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
     DATABASES = {
         'default': dj_database_url.config(default=config('DATABASE_URL'))
@@ -113,11 +113,12 @@ else:
             'PORT': config('POSTGRES_PORT'),
         }
     }
-    STATIC_URL = "/static/"
-    MEDIA_URL = "/media/"
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-    STATIC_ROOT = DATA_DIR / "static"
-    MEDIA_ROOT = DATA_DIR / "media"
+
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATIC_ROOT = DATA_DIR / "static"
+MEDIA_ROOT = DATA_DIR / "media"
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
