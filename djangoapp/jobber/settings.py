f9d436f86cdc7bb6ea01a903e1021330d981f677
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 18:21:53 -0300

    Fix static files settings

--- djangoapp/jobber/settings.py
+++ djangoapp/jobber/settings.py
@@ -109,11 +109,12 @@ AWS_S3_FILE_OVERWRITE = False
 
 
 # STATIC FILES
+if ON_HEROKU:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 STATIC_ROOT = DATA_DIR / "static"
