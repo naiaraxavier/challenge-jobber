b87265bc330d0340f56ca0faa75844e28b29fe68
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 15:49:49 -0300

    update procfile and settings

--- djangoapp/jobber/settings.py
+++ djangoapp/jobber/settings.py
@@ -185,12 +185,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # STATIC_URL = "/static/"
-# # /data/web/static
-# STATIC_ROOT = DATA_DIR / "static"
+# /data/web/static
+STATIC_ROOT = DATA_DIR / "static"
 
-# MEDIA_URL = "/media/"
-# # /data/web/media
-# MEDIA_ROOT = DATA_DIR / "media"
+MEDIA_URL = "/media/"
+# /data/web/media
+MEDIA_ROOT = DATA_DIR / "media"
 
 REST_FRAMEWORK = {
     "DEFAULT_DATETIME_FORMAT": "d/m/Y",
@@ -201,30 +201,3 @@ REST_FRAMEWORK = {
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-# # Configuração para upload de arquivos
-# FILE_UPLOAD_STORAGE = config("FILE_UPLOAD_STORAGE", default="local")
-
-# if FILE_UPLOAD_STORAGE == "s3":
-#     # Configuração de armazenamento S3
-#     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-#     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-#     AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-#     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-#     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-# else:
-#     STATIC_URL = "/static/"
-#     # /data/web/static
-#     STATIC_ROOT = DATA_DIR / "static"
-
-#     MEDIA_URL = "/media/"
-#     # /data/web/media
-#     MEDIA_ROOT = DATA_DIR / "media"
-
-# AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME", default="sa-east-1")
-# AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-# AWS_DEFAULT_ACL = None
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_QUERYSTRING_AUTH = False
