0fa5fd2496a636443b6ce9f415bd899560270060
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 16:15:59 -0300

    update settings, sh and procfile

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,15 +5,16 @@ set -e
 
 echo "PORT is set to: $PORT"
 
-# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-#   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-#   sleep 2
-# done
-
-# echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
-
-
+echo "Running collectstatic..."
 python manage.py collectstatic --noinput
+
+echo "Running makemigrations..."
 python manage.py makemigrations --noinput
+
+echo "Running migrate..."
 python manage.py migrate --noinput
+
+echo "Running runserver..."
 python manage.py runserver 0.0.0.0:$PORT
+
+echo "Commands completed."
