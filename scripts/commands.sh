9aad956ee48b0e0cd9f763e02d519fd119ad8225
Naiara Martins <naiaraxf@gmail.com>
2024-09-12 22:48:30 -0300

    update sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -7,17 +7,17 @@ echo "PORT is set to: $PORT"
 
 # cd djangoapp
 
-# echo "Running collectstatic..."
-# python manage.py collectstatic --noinput
+echo "Running collectstatic..."
+python manage.py collectstatic --noinput
 
-# echo "Running makemigrations..."
-# python manage.py makemigrations --noinput
+echo "Running makemigrations..."
+python manage.py makemigrations --noinput
 
-# echo "Running migrate..."
-# python manage.py migrate --noinput
+echo "Running migrate..."
+python manage.py migrate --noinput
 
 echo "Running runserver..."
-# python manage.py runserver 0.0.0.0:$PORT
-gunicorn jobber.wsgi:application --bind 0.0.0.0:$PORT
+python manage.py runserver 0.0.0.0:$PORT
+# gunicorn jobber.wsgi:application --bind 0.0.0.0:$PORT
 
 echo "Commands completed."
